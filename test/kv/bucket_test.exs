00e36f8d0042c9ea83aca8cd0e6052fd1980fe5b
Fabrizio Monti <fabrizio.monti@welaika.com>
2018-05-06 18:42:02 +0200

    Use a test callback ( like a before) to set a state for all tests in file

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,8 +1,12 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     { :ok, bucket } = KV.Bucket.start_link()
+    %{ bucket: bucket }
+  end
+
+  test "stores values by key", %{ bucket: bucket } do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 5)
